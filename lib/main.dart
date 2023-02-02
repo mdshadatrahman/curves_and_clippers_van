ddfeaf884149c8b04dd95c44b4d6ba5e1572cfa3
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-02 15:07:47 +0600

    container

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white30,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: const Color(0xff0057b7),
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: const Color(0xffffd700),
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
