b1397cdab2bab43f78c0d83c6002d3ba43b279ed
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-02-02 15:02:51 +0600

    init

---
+++ android/app/src/main/kotlin/me/shadatrahman/curves_and_clippers/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.shadatrahman.curves_and_clippers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white30,
+    );
+  }
+}
